b3e2d9bd643b47aea98317b5e5da3a567504e761
dev4us <bronxofsoul@gmail.com>
2020-01-14 12:34:04 +0900

    # BE 7. create graphql Types

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,21 +1,8 @@
 import { GraphQLServer } from "graphql-yoga";
 import connection from "./ormConfig";
+import schema from "./schema";
 
-//인자값과 리턴되는 값의 타입을 지정
-const typeDefs = `
-  type Query{
-    sayHello: String!
-  }
-`;
-
-//함수의 비지니스로직 실제로 들어가는 부분
-const resolvers = {
-  Query: {
-    sayHello: () => "Hi there :0)"
-  }
-};
-
-const server = new GraphQLServer({ typeDefs, resolvers });
+const server = new GraphQLServer({ schema });
 
 connection.then(() =>
   server.start(() =>
