bfffac65bfa43589267ab6855770b20a622a6c9c
dev4us <bronxofsoul@gmail.com>
2020-01-21 12:39:17 +0900

    # BE 9. set Graphql to typescript Fnc types

--- backend/src/api/Message/GetMessages/GetMessages.resolvers.ts
+++ backend/src/api/Message/GetMessages/GetMessages.resolvers.ts
@@ -1,9 +1,13 @@
 import { Resolvers } from "src/types/resolvers";
 import Message from "../../../../src/entities/Message";
+import { GetMessagesQueryArgs, GetMessagesResponse } from "src/types/graphql";
 
 const resolvers: Resolvers = {
   Query: {
-    GetMessages: async (_, args) => {
+    GetMessages: async (
+      _,
+      args: GetMessagesQueryArgs
+    ): Promise<GetMessagesResponse> => {
       //ㅗ직 시작
       try {
         const { innerChannelId } = args;
