55e1d6d826bc134be1eb043ce3f71c51fd74321b
dev4us <bronxofsoul@gmail.com>
2020-02-13 12:51:54 +0900

    frontend 배포배포

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,19 @@
 import React from "react";
+import styled from "styled-components";
+import ChannelList from "./Components/ChannelList";
 
 function App() {
-  return <>hello jongwon ; )</>;
+  return (
+    <MainFrame>
+      <ChannelList></ChannelList>
+    </MainFrame>
+  );
 }
+
+const MainFrame = styled.div`
+  display: flex;
+  height: 100%;
+  flex-direction: row;
+`;
 
 export default App;
--- frontend/src/Potato.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const Potato = ({ mode }) => {
-  return <div>{mode}</div>;
-};
-
-export default Potato;
